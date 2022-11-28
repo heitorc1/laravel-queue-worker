a9a96f4f041dbac53207cde0f6927b1088660df3
Heitor Carneiro <heitorcarneiro1@gmail.com>
2022-11-27 21:49:28 -0300

    change unity limits

--- app/Library/SourceAPI.php
+++ app/Library/SourceAPI.php
@@ -3,17 +3,14 @@
 namespace App\Library;
 
 use App\Jobs\ProcessData;
-use App\Models\Data;
-use GuzzleHttp\Client;
 
 class SourceAPI
 {
   public function process()
   {
     try {
-      $setores = explode(",", env('SETORES'));
-      foreach ($setores as $setor) {
-        ProcessData::dispatch($setor);
+      for ($i = 1; $i <= 10000; $i++) {
+        ProcessData::dispatch($i);
       }
     } catch (\Exception $e) {
       throw $e;
